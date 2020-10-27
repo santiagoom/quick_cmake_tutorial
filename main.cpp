98e8a444e36ea7caa8e29c50e7cea5453a565417
santiagoom <ingenuity_zero@163.com>
2020-10-27 16:47:11 +0800

    update config

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-//#include "calc.h"
+#include "calc.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-//    std::cout << add_values(1, 2) << std::endl;
+    std::cout << add_values(1, 2) << std::endl;
     return 0;
 }
